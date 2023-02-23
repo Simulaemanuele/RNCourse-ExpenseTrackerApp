70da8a223c0e09d89d9cb7f3c902b99c24c1a104
Emanuele Simula <simulaemanuele@gmail.com>
2023-02-23 10:28:01 +0100

    feat: added and configured the navigation Stack and Bottom Tab nested.

--- App.tsx
+++ App.tsx
@@ -6,13 +6,44 @@
  */
 
 import * as React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+
+const Stack = createNativeStackNavigator();
+const Bottomtabs = createBottomTabNavigator();
+
+const ExpensesOverview = () => {
+  return (
+    <Bottomtabs.Navigator>
+      <Bottomtabs.Screen name="RecentExpenses" component={RecentExpenses} />
+      <Bottomtabs.Screen name="AllExpenses" component={AllExpenses} />
+    </Bottomtabs.Navigator>
+  );
+};
 
 function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Expense Tracker App!!</Text>
-    </View>
+    <>
+      <StatusBar barStyle="default" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
