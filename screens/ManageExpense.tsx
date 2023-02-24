1db286b54b6103e358c84672848a2a2ed882033b
Emanuele Simula <simulaemanuele@gmail.com>
2023-02-24 13:12:28 +0100

    feat: created a Button.tsx component highly customizeable, used in MenageExpense screen to implement add, cancel and confirm features; logic should be implemented

--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -1,6 +1,7 @@
 import {NavigationProp, RouteProp} from '@react-navigation/native';
 import * as React from 'react';
 import {Text, StyleSheet, View} from 'react-native';
+import Button from '../components/UI/Button';
 import IconButton from '../components/UI/IconButton';
 import {GlobalStyles} from '../constants/styles';
 
@@ -23,8 +24,20 @@ const ManageExpense = ({
 
   const deleteExpenseHandler = () => {};
 
+  const cancelHandler = () => {};
+
+  const confirmHandler = () => {};
+
   return (
     <View style={styles.container}>
+      <View style={styles.buttons}>
+        <Button mode="flat" onPress={cancelHandler} style={styles.button}>
+          Cancel
+        </Button>
+        <Button mode="none" onPress={confirmHandler} style={styles.button}>
+          {isEditing ? 'Update' : 'Add'}
+        </Button>
+      </View>
       {isEditing && (
         <View style={styles.deleteContainer}>
           <IconButton
@@ -46,6 +59,15 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
   },
   deleteContainer: {
     marginTop: 16,
