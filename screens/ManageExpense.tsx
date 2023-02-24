c1e70bf745a11345aa09d44c976d43ca180c63ea
Emanuele Simula <simulaemanuele@gmail.com>
2023-02-24 12:30:55 +0100

    feat: added a conditional IconButton with a delete handler only for Edit feature.

--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -1,6 +1,8 @@
 import {NavigationProp, RouteProp} from '@react-navigation/native';
 import * as React from 'react';
-import {Text, StyleSheet} from 'react-native';
+import {Text, StyleSheet, View} from 'react-native';
+import IconButton from '../components/UI/IconButton';
+import {GlobalStyles} from '../constants/styles';
 
 const ManageExpense = ({
   route,
@@ -19,9 +21,37 @@ const ManageExpense = ({
     });
   }, [navigation, isEditing]);
 
-  return <Text>Manage Expense!</Text>;
+  const deleteExpenseHandler = () => {};
+
+  return (
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 };
 
 export default ManageExpense;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    borderTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center',
+  },
+});
